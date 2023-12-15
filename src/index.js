334ea7aadfa8f0324a6ec77f97d1a7e72c4305c7
Błażej Bartoszewski <blazejbart@gmail.com>
2023-12-15 01:24:42 +0100

    feat apdate index.js & cat-api.js

--- src/index.js
+++ src/index.js
@@ -1,51 +1,51 @@
-import { fetchCatByBreed } from './cat-api';
-import axios from 'axios';
-
-function fetchBreeds() {
-  return new Promise((resolve, reject) => {
-    const breedSelect = document.querySelector('.breed-select');
-    const loader = document.querySelector('.loader');
-    const error = document.querySelector('.error');
-
-    breedSelect.style.display = 'none';
-    loader.style.display = 'block';
-    error.style.display = 'none';
-
-    axios
-      .get('https://api.thecatapi.com/v1/breeds')
-      .then(response => {
-        const breeds = response.data;
-        const options = breeds
-          .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
-          .join('');
-        breedSelect.innerHTML = options;
-
-        breedSelect.style.display = 'block';
-        loader.style.display = 'none';
-        error.style.display = 'none';
-        resolve(breeds);
-      })
-      .catch(err => {
-        breedSelect.style.display = 'none';
-        loader.style.display = 'none';
-        error.style.display = 'block';
-        reject(err);
-      });
-  });
-}
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const breedSelect = document.querySelector('.breed-select');
+
   fetchBreeds()
     .then(breeds => {
-      const breedSelect = document.querySelector('.breed-select');
-      breedSelect.addEventListener('change', event => {
-        const selectedBreedId = event.target.value;
-        if (selectedBreedId) {
-          fetchCatByBreed(selectedBreedId);
-        }
-      });
+      const options = breeds
+        .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
+        .join('');
+      breedSelect.innerHTML = options;
     })
     .catch(error => {
       console.error(error);
     });
+
+  breedSelect.addEventListener('change', event => {
+    const selectedBreedId = event.target.value;
+    if (selectedBreedId) {
+      fetchCatByBreed(selectedBreedId)
+        .then(cat => {
+          const catInfo = document.querySelector('.cat-info');
+          const loader = document.querySelector('.loader');
+          const error = document.querySelector('.error');
+
+          catInfo.style.display = 'none';
+          loader.style.display = 'block';
+          error.style.display = 'none';
+
+          const { name, description, temperament } = cat.breeds[0];
+          const image = `<img class="cat-image" src="${cat.url}" alt="${name}" width="800px" height="600px">`;
+          const info = `<h2>${name}</h2><p><strong>Description:</strong> ${description}</p><p><strong>Temperament:</strong> ${temperament}</p>`;
+          catInfo.innerHTML = image + info;
+
+          catInfo.style.display = 'block';
+          loader.style.display = 'none';
+          error.style.display = 'none';
+        })
+        .catch(error => {
+          const catInfo = document.querySelector('.cat-info');
+          const loader = document.querySelector('.loader');
+          const errorElement = document.querySelector('.error');
+
+          catInfo.style.display = 'none';
+          loader.style.display = 'none';
+          errorElement.style.display = 'block';
+          console.error(error);
+        });
+    }
+  });
 });
