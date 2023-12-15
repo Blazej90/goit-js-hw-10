1e4f6e36623214230a11cffd2f317966b25935d7
Błażej Bartoszewski <blazejbart@gmail.com>
2023-12-15 03:22:53 +0100

    feat: index.js and api-cat.js

--- src/index.js
+++ src/index.js
@@ -1,51 +1,28 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let breedsData;
+
   const breedSelect = document.querySelector('.breed-select');
+  breedSelect.addEventListener('change', event => {
+    const selectedBreedId = event.target.value;
+    if (selectedBreedId) {
+      fetchCatByBreed(selectedBreedId);
+    }
+  });
 
   fetchBreeds()
     .then(breeds => {
+      breedsData = breeds;
       const options = breeds
         .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
         .join('');
       breedSelect.innerHTML = options;
+      breedSelect.style.display = 'block';
+      document.querySelector('.loader').style.display = 'none';
     })
     .catch(error => {
       console.error(error);
+      document.querySelector('.error').style.display = 'block';
     });
-
-  breedSelect.addEventListener('change', event => {
-    const selectedBreedId = event.target.value;
-    if (selectedBreedId) {
-      fetchCatByBreed(selectedBreedId)
-        .then(cat => {
-          const catInfo = document.querySelector('.cat-info');
-          const loader = document.querySelector('.loader');
-          const error = document.querySelector('.error');
-
-          catInfo.style.display = 'none';
-          loader.style.display = 'block';
-          error.style.display = 'none';
-
-          const { name, description, temperament } = cat.breeds[0];
-          const image = `<img class="cat-image" src="${cat.url}" alt="${name}" width="800px" height="600px">`;
-          const info = `<h2>${name}</h2><p><strong>Description:</strong> ${description}</p><p><strong>Temperament:</strong> ${temperament}</p>`;
-          catInfo.innerHTML = image + info;
-
-          catInfo.style.display = 'block';
-          loader.style.display = 'none';
-          error.style.display = 'none';
-        })
-        .catch(error => {
-          const catInfo = document.querySelector('.cat-info');
-          const loader = document.querySelector('.loader');
-          const errorElement = document.querySelector('.error');
-
-          catInfo.style.display = 'none';
-          loader.style.display = 'none';
-          errorElement.style.display = 'block';
-          console.error(error);
-        });
-    }
-  });
 });
